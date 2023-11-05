cbeb5f53aa28bccafc59e2821dbd4a4f0dda7f6d
MauricioUT <mauricio.utrilla.d@hotmail.com>
2023-11-05 15:17:05 -0600

    create event was changed

--- src/main/java/mx/sooner/citas/util/CalendarQuickstart.java
+++ src/main/java/mx/sooner/citas/util/CalendarQuickstart.java
@@ -1,30 +1,28 @@
 package mx.sooner.citas.util;
 
 import com.google.api.client.auth.oauth2.Credential;
-import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
-import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
-import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
-import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.auth.oauth2.*;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.gson.GsonFactory;
 import com.google.api.client.util.DateTime;
-import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.client.util.store.MemoryDataStoreFactory;
 import com.google.api.services.calendar.Calendar;
 import com.google.api.services.calendar.CalendarScopes;
 import com.google.api.services.calendar.model.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.stereotype.Component;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.security.GeneralSecurityException;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.Scanner;
 
-/* class to demonstrate use of Calendar events list API */
+@Component
 public class CalendarQuickstart {
     /**
      * Application name.
@@ -46,13 +44,19 @@ public class CalendarQuickstart {
     private static final List<String> SCOPES = Collections.singletonList(CalendarScopes.CALENDAR);
 
     private static final String CREDENTIALS_FILE_PATH = "/credentials.json";
+    private ResourceLoader resourceLoader;
+
+    @Autowired
+    public CalendarQuickstart(ResourceLoader resourceLoader) {
+        this.resourceLoader = resourceLoader;
+    }
 
     /**
      * Creates an authorized Credential object.
      *
      * @param HTTP_TRANSPORT The network HTTP Transport.
      * @return An authorized Credential object.
-     * @throws IOException If the credentials.json file cannot be found.
+     * @throws IOException If the credentials.bk.json file cannot be found.
      */
     private static Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT)
             throws IOException {
@@ -65,14 +69,31 @@ public class CalendarQuickstart {
                 GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
 
         // Build flow and trigger user authorization request.
-        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
+        /*GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
                 HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
                 .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
                 .setAccessType("offline")
                 .build();
         LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
-        Credential credential = new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
-        //returns an authorized Credential object.
+        Credential credential = new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");*/
+
+        GoogleAuthorizationCodeFlow authorizationCodeFlow = new GoogleAuthorizationCodeFlow
+                .Builder(HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
+                .setCredentialDataStore(new MemoryDataStoreFactory().getDataStore("tokens"))
+                .build();
+        String userId = "user-id";
+        Credential credential = authorizationCodeFlow.loadCredential(userId);
+        if (credential == null) {
+            GoogleAuthorizationCodeRequestUrl authorizationUrl = authorizationCodeFlow.newAuthorizationUrl();
+            authorizationUrl.setRedirectUri(GoogleOAuthConstants.OOB_REDIRECT_URI);
+            System.out.println("Please, authorize application. Visit {} " + authorizationUrl);
+            Scanner s = new Scanner(System.in);
+            String code = s.nextLine();
+            GoogleAuthorizationCodeTokenRequest tokenRequest = authorizationCodeFlow.newTokenRequest(code);
+            tokenRequest.setRedirectUri(GoogleOAuthConstants.OOB_REDIRECT_URI);
+            GoogleTokenResponse tokenResponse = tokenRequest.execute();
+            credential = authorizationCodeFlow.createAndStoreCredential(tokenResponse, userId);
+        }
         return credential;
     }
 
@@ -95,7 +116,7 @@ public class CalendarQuickstart {
         List<Event> items = events.getItems();
         if (items.isEmpty()) {
             System.out.println("No upcoming events found.");
-            return createEvent(service,mail,startDate,endDate);
+            return createEvent(service, mail, startDate, endDate);
         } else {
             System.out.println("Upcoming events");
             for (Event event : items) {
@@ -105,16 +126,15 @@ public class CalendarQuickstart {
                 }
                 System.out.printf("%s (%s)\n", event.getSummary(), start);
             }
-            return createEvent(service,mail,startDate,endDate);
+            return createEvent(service, mail, startDate, endDate);
         }
     }
 
     /**
-     *
      * @param service
      * @param mail
      * @param startDate "2023-10-31T22:00:00-07:00"
-     * @param endDate "2023-10-31T22:00:00-07:00"
+     * @param endDate   "2023-10-31T22:00:00-07:00"
      * @return
      * @throws IOException
      */
