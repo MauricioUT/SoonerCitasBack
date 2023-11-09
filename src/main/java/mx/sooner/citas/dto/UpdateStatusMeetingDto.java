8168760b637a159f527ead2bc633f373a2699b62
MauricioUT <mauricio.utrilla.d@hotmail.com>
2023-11-08 18:32:19 -0600

    imports were deleted

--- src/main/java/mx/sooner/citas/dto/UpdateStatusMeetingDto.java
+++ src/main/java/mx/sooner/citas/dto/UpdateStatusMeetingDto.java
@@ -6,7 +6,6 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.io.Serializable;
-import java.util.UUID;
 
 @Setter
 @Getter
@@ -16,7 +15,7 @@ public class UpdateStatusMeetingDto implements Serializable {
 
     private static final long serialVersionUID = 810685864285664513L;
 
-    private String id;
+    private String uuid;
     private String observations;
     private boolean status;
 }
