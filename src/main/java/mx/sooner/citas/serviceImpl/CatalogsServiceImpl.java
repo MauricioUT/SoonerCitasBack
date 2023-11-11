c6d80e734d51b48264e31e65218f26a7900b3989
MauricioUT <mauricio.utrilla.d@hotmail.com>
2023-11-10 18:13:29 -0600

    return empty list when attention schedule will be empty

--- src/main/java/mx/sooner/citas/serviceImpl/CatalogsServiceImpl.java
+++ src/main/java/mx/sooner/citas/serviceImpl/CatalogsServiceImpl.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Service;
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -89,7 +90,8 @@ public class CatalogsServiceImpl implements CatalogsService {
             cAttentionSchedules = cAttentionScheduleRepository.findScheduleByDateWeekly(localDate, idCentroEval, Long.valueOf(evaluationCenter.get().getNoMaxMeetings()));
 
         if (cAttentionSchedules.isEmpty())
-            return new ResponseEntity<>(null, HttpStatus.NO_CONTENT);
+            return new ResponseEntity<>(new ArrayList<>(), HttpStatus.OK);
+           // return new ResponseEntity<>(null, HttpStatus.NO_CONTENT);
         List<CatalogDto> catalog = cAttentionSchedules.stream().map(ca -> {
             CatalogDto cat = new CatalogDto();
             cat.setId(ca.getId());
